0631d14626f530d4248859f487ec6c886f03d746
Dustin Huntsberger <dhunts258@gmail.com>
2022-02-21 17:00:09 -0500

    bug fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // Assignment code here
 var specialCharacters = ["!",'”',"#","$","%","&","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
-var upperCaseCharaters = ["A",'B',"C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var upperCaseCharacters = ["A",'B',"C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var lowerCaseCharacters = ["a",'b',"c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var numericCharacters = ["0","1","2","3","4","5","6","7","8","9"];
 
@@ -49,10 +49,11 @@ function generatePassword() {
         var password = "";
         var digit = "";
         for (var i = 0; i < lengthPasswordNumber; i++) {
-            digit = totalCriteria[Math.floor(Math.random()*totalCriteria.length())];
+            digit = totalCriteria[Math.floor(Math.random() * totalCriteria.length )];
             password += digit;
             console.log(password);
         }
+        return(password);
     }
 }
 
